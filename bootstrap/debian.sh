33b5082a699373d71a8f5cc64db9debfbaa1348f
Ben Spencer <ben.spencer@healthforge.io>
2018-01-20 14:22:16 +0000

    Initial commit.

---
+++ bootstrap/debian.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+if [ $# -lt 1 ]; then
+   echo "Usage: $0 <host>"
+   exit 1
+fi
+
+host=$1
+local_user="ansible"
+
+ssh root@$host /bin/sh -e <<EOF
+useradd -m $local_user
+mkdir /home/$local_user/.ssh
+apt-get update
+apt-get -y install sudo
+sed -i 's/^%sudo.*/%sudo ALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers
+adduser $local_user sudo
+EOF
+
+scp ~/.ssh/id_rsa.pub root@$host:/home/$local_user/.ssh/authorized_keys
+
+ssh root@$host /bin/sh -e <<EOF
+chown -R $local_user: /home/$local_user
+chmod 0600 /home/$local_user/.ssh/authorized_keys
+EOF
